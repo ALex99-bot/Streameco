51532faef95970339c0989add9e14c0b7a141304
pedro <pedroalex123@gmail.com>
2023-03-09 00:17:22 +0000

    matriz para PCA

--- fungi&bact_final.R
+++ fungi&bact_final.R
@@ -1,12 +1,14 @@
 # Set the directory and file name
 directory <- "/home/pedro/PycharmProjects/Streameco"
+setwd(directory)
+
 file <- "total_reads.xlsx"
 file2 <- "top10.xlsx"
 file3 <- "menor_cutoff_reads.xlsx"
 path <- file.path(directory, file)
 path2 <- file.path(directory, file2)
 path3 <- file.path(directory, file3)
-setwd(directory)
+
 
 # Load the required packages
 library(readxl)
@@ -17,10 +19,13 @@ library(vegan)
 library(ggplot2)
 library(ggrepel)
 
+read_excel_sheets <- function(path, file){
+  my_sheet_names <- excel_sheets(path)
+  my_sheets <- lapply(my_sheet_names, function(x) read_excel(file, sheet = x))
+  names(my_sheets) <- my_sheet_names
+  return(my_sheets)
+}
 
-my_sheet_names <- excel_sheets(path3)
-my_sheets <- lapply(my_sheet_names, function(x) read_excel(file3, sheet = x))
-names(my_sheets) <- my_sheet_names
 
 excel2dataframe <- function(x) {
   x <- as.data.frame(x)
@@ -28,108 +33,125 @@ excel2dataframe <- function(x) {
   x <- x[, -1]
 }
 
-new_list <- lapply(my_sheets, excel2dataframe)
+bact_fung_taxa <- lapply(read_excel_sheets(path, file), excel2dataframe)
 
-# list2env(new_list, envir=.GlobalEnv)
+# list2env(bact_fung_taxa, envir=.GlobalEnv)
 
-nmds <- lapply(new_list, metaMDS, distance = "bray")
-
-# organise4plot <- function (nmds) {
-#   Site <- as.data.frame(nmds$points)
-#   Site <- as.data.frame(cbind(Site, name="Species"))
-#   Species <- as.data.frame(nmds$species)
-#   Species <- as.data.frame(cbind(Species, name="Site"))
+# nmds <- lapply(new_list, metaMDS, distance = "bray")
+#
+# # organise4plot <- function (nmds) {
+# #   Site <- as.data.frame(nmds$points)
+# #   Site <- as.data.frame(cbind(Site, name="Species"))
+# #   Species <- as.data.frame(nmds$species)
+# #   Species <- as.data.frame(cbind(Species, name="Site"))
+# #   df <- rbind(Site, Species)
+# # }
+#
+# prepare_nmds_data <- function(nmds) {
+#   if (!is.numeric(nmds$points) || !is.numeric(nmds$species)) {
+#     stop("Input must be a list with 'points' and 'species' as numeric data frames")
+#   }
+#
+#   Site <- data.frame(nmds$points, name="Species")
+#   Species <- data.frame(nmds$species, name="Site")
 #   df <- rbind(Site, Species)
+#   return(df)
 # }
+#
+# # all_nmds <- lapply(nmds, prepare_nmds_data)
+# all_nmds <- sapply(nmds, FUN = prepare_nmds_data, simplify = FALSE, USE.NAMES = TRUE)
+#
+# plot_text = function (data, df_name) {
+#     ggplot(data, aes(x=MDS1, y=MDS2)) +
+#     geom_text_repel(aes(color=name, label=row.names(data)))+labs(col="Legend")+
+#     guides(fill="name") +
+#     theme_gray() +
+#     labs(title=df_name)+
+#     scale_color_manual(labels = c("Site", strsplit(df_name, split = "_")[[1]][2]), values = c("red", "blue"))
+# }
+#
+# scatter_plot = function (data, df_name) {
+#   ggplot() +
+#   geom_point(data = data, aes(x = MDS1, y = MDS2, color = name)) +
+#   labs(col = "Legend") +
+#   guides(fill = "name") +
+#   theme_gray() +
+#   labs(title = df_name) +
+#   scale_color_manual(labels = c("Site", strsplit(df_name, split = "_")[[1]][2]), values = c("red", "blue"))
+# }
+#
+# myplots_text <- lapply(names(all_nmds), function(df_name) {
+#   plot_text(all_nmds[[df_name]], df_name)
+# })
+#
+# myplots_scatter <- lapply(names(all_nmds), function(df_name) {
+#   scatter_plot(all_nmds[[df_name]], df_name)
+# })
+#
+# pdf("top10_nomes.pdf")
+# for (i in 1:length(myplots_text)) {
+#   print(myplots_text[[i]])
+# }
+# dev.off()
+#
+# pdf("top10_pontos.pdf")
+# for (i in 1:length(myplots_scatter)) {
+#   print(myplots_scatter[[i]])
+# }
+# dev.off()
 
-prepare_nmds_data <- function(nmds) {
-  if (!is.numeric(nmds$points) || !is.numeric(nmds$species)) {
-    stop("Input must be a list with 'points' and 'species' as numeric data frames")
+
+shannon_by_column <- function(data, name) {
+  # Create an empty data frame to store the results
+  shannon_df <- data.frame(shannon = numeric(), row.names = character(), stringsAsFactors = FALSE)
+  for(i in 1:ncol(data)) {       # for-loop over columns
+  shannon <- data.frame(shannon = diversity(t(data[,i]), index = "shannon", MARGIN = 1, base = exp(1)),
+                         row.names = colnames(data)[i])
+  # Add the current result to the shannon_df data frame
+  shannon_df <- rbind(shannon_df, shannon)
   }
-
-  Site <- data.frame(nmds$points, name="Species")
-  Species <- data.frame(nmds$species, name="Site")
-  df <- rbind(Site, Species)
-  return(df)
+  # Name of dataframe
+  name_col <- paste("shannon_index", sub("_([^_]*)$", "", name), sep="_")
+  colnames(shannon_df)[1] <- name_col
+  return(shannon_df)
 }
 
-# all_nmds <- lapply(nmds, prepare_nmds_data)
-all_nmds <- sapply(nmds, FUN = prepare_nmds_data, simplify = FALSE, USE.NAMES = TRUE)
-
-plot_text = function (data, df_name) {
-    ggplot(data, aes(x=MDS1, y=MDS2)) +
-    geom_text_repel(aes(color=name, label=row.names(data)))+labs(col="Legend")+
-    guides(fill="name") +
-    theme_gray() +
-    labs(title=df_name)+
-    scale_color_manual(labels = c("Site", strsplit(df_name, split = "_")[[1]][2]), values = c("red", "blue"))
-}
-
-scatter_plot = function (data, df_name) {
-  ggplot() +
-  geom_point(data = data, aes(x = MDS1, y = MDS2, color = name)) +
-  labs(col = "Legend") +
-  guides(fill = "name") +
-  theme_gray() +
-  labs(title = df_name) +
-  scale_color_manual(labels = c("Site", strsplit(df_name, split = "_")[[1]][2]), values = c("red", "blue"))
-}
-
-myplots_text <- lapply(names(all_nmds), function(df_name) {
-  plot_text(all_nmds[[df_name]], df_name)
+# Índice de Shannnon
+shannon <- lapply(names(bact_fung_taxa), function(df_name) {
+  shannon_by_column(bact_fung_taxa[[df_name]], df_name)
 })
-
-myplots_scatter <- lapply(names(all_nmds), function(df_name) {
-  scatter_plot(all_nmds[[df_name]], df_name)
-})
-
-pdf("menor_NMDS_nomes.pdf")
-for (i in 1:length(myplots_text)) {
-  print(myplots_text[[i]])
-}
-dev.off()
-
-pdf("menor_NMDS_disp.pdf")
-for (i in 1:length(myplots_scatter)) {
-  print(myplots_scatter[[i]])
-}
-dev.off()
-
-
-
-# # Índice de Shannnon e Simpson bactérias
 # shannon_bact <- diversity(t(bact_reads), index = "shannon", MARGIN = 1, base = exp(1))
 # shannon_fung <- diversity(t(fung_reads), index = "shannon", MARGIN = 1, base = exp(1))
+
+
+# bray curtis 1st axis of PCoA
+# Functional space
+bray_curtis <- lapply(t(bact_fung_taxa), vegdist, method = "bray")
+# _bact <- vegdist(t(bact_reads), method = "bray")
+# dudi.pco(bray_curtis_bact,scannf = F,nf=10)-> tr.pco
 #
-#
-#
-# # bray curtis 1st axis of PCoA
-# # Functional space
-# bray_curtis_bact <- vegdist(t(bact_reads), method = "bray")
-# dudi.pco(bray_curtis_bact,scannf = F,nf=10)->tr.pco
-#
-# Bray_Curtis_bact<-tr.pco$li$A1
+# Bray_Curtis_bact <- tr.pco$li$A1
 #
 # bray_curtis_fung <- vegdist(t(fung_reads), method = "bray")
-# dudi.pco(bray_curtis_fung,scannf = F,nf=10)->tr.pco2
+# dudi.pco(bray_curtis_fung,scannf = F,nf=10) -> tr.pco2
 # Bray_Curtis_fung<-tr.pco2$li$A1
 #
 # div <- cbind(div, shannon_bact, Bray_Curtis_bact, shannon_fung, Bray_Curtis_fung)
 # div <- as.data.frame(div)
 #
 #
-# env.data <- read.table("var ambientais.txt", sep="\t", dec=".", header=T)
-# row.names(env.data) <- env.data$code
-# env.data <- env.data[, -1]
-# View(env.data)
-#
-#
-#
-# env.data <- as.data.frame(env.data)
-# env.data2 <- env.data[, c(12, 25, 32, 37)]
-# env.data <- env.data[, -c(12, 25, 32, 37)]
-# row.names(env.data2)<-row.names(env.data)
+env.data <- read.table("var ambientais.txt", sep="\t", dec=".", header=T)
+row.names(env.data) <- env.data$code
+env.data <- env.data[, -1]
+View(env.data)
 
+env.data <- as.data.frame(env.data)
+env.data2 <- env.data[, c(12, 25, 32, 37)]
+env.data <- env.data[, -c(12, 25, 32, 37)]
+row.names(env.data2) <- row.names(env.data)
+
+
+matriz_final <- Reduce(function(df1, df2) cbind(df1, df2), shannon, init = env.data)
 
 
 # div <- as.data.frame(scale(div))
